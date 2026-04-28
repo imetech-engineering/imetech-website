5d563683e36884f775d2df27c0e16a9a3b213cb9
imetech-engineering <info@imetech.nl>
2026-04-28 13:13:34 +0200

    Fix cookies banner

--- script.js
+++ script.js
@@ -47,7 +47,7 @@
     window.__imetechGtagLoaded = true;
 
     window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
+    function gtag() { window.dataLayer.push(arguments); }
     window.gtag = gtag;
     gtag('js', new Date());
     gtag('config', GA_MEASUREMENT_ID);
